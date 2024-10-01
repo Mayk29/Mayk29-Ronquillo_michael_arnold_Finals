b09b5cdf566638474cb0b2a457988f28bba12ebd
Mayk29 <144456367+Mayk29@users.noreply.github.com>
2024-10-01 23:56:30 +0800

    Create about.vue

---
+++ view_router_demo/finalprj/src/components/about.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="about">
+     
+     
+      <img src="../assets/naruto-about.png" class="img-about">
+      <h1>Introduction</h1>
+      <p>Naruto is a Japanese manga (comic or graphic novel) adventure series that was originally published in print from 1999 to 2014. The story follows a young ninja, Naruto Uzumaki, on his journey to become the Hokage, or head ninja, of his village. The manga was first released in installments in the Japanese magazine Weekly Shonen Jump. Publishers later collected the installments and presented them in book form. Naruto was translated into numerous languages, helping to spread its popularity. The Naruto anime, or animated television series, aired in the early 21st century. Other related items include video games and a trading card game.</p>
+      <h1>Manga</h1>
+      <p>Japanese artist Masashi Kishimoto created, wrote, and illustrated the Naruto manga series. He wrote each of the 700 chapters, or episodes, in Japanese. Reviewers praised his work for the strong story lines, well-developed characters, and detailed artwork. After the initial success of the story, the works were translated into other languages. For example, the series appeared in English in the manga magazine Weekly Jump—the English equivalent of Weekly Shonen Jump—from 2003 to 2015. After the serialized release in magazines, the chapters were collected and published as books. By the early 2020s more than 250 million print copies of Naruto existed in more than 40 countries. In addition, both Kishimoto and other authors created original novels chronicling the adventures of some of the characters from the series.
+
+Kishimoto split the series into two parts. The first part explores the life of 12-year-old Naruto, who was orphaned as a child. The people in the village where Naruto was born fear and ridicule him. He eventually finds out that he has a demon, the Nine-Tailed Fox, inside him. His father, who at the time was Hokage, imprisoned the demon inside Naruto to save the village. Meanwhile, in order to obtain the respect of the villagers, Naruto goes to a special academy to train as a ninja. Along the way he learns to work as a team with fellow ninjas Sasuke and Sakura, who become major characters in the series.
+
+The second part of the series occurs a few years later, after the young ninjas have completed their training. During this time Naruto releases the demon fox from inside him and defeats it. He also navigates sinister events and fights against evil beasts, former ninjas, and the misguided Sasuke, who has become consumed with revenge.
+
+Kishimoto created a sequel manga series called Boruto: Naruto Next Generations in 2016. It tracks the ninja adventures of Boruto, Naruto’s son. Ukyo Kodachi wrote the first 13 volumes, and then Kishimoto took on that responsibility. Mikio Ikemoto illustrated the entire series.
+</p>
+<h1>Anime</h1>
+<p>In 2002 an anime version of Naruto began airing on television in Japan. It encompassed part one of the manga series and lasted until 2007. More than half of the 220 episodes followed the written story, while the other episodes involved new story lines. The show Naruto: Shippuden, which consisted of part two of the manga series, aired immediately after the first series ended. It lasted until 2017. The animated series dubbed in English began streaming in the United States in 2005. A sequel, Boruto: Naruto Next Generations, debuted on Japanese television in 2017. It appeared in the United States with English subtitles in 2018. Several movies helped to further the story of the ninjas. Naruto the Movie: Ninja Clash in the Land of Snow (2004) was released with English subtitles in North America in 2007 and Naruto Shippuden the Movie: The Lost Tower (2010) in 2013.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'about',
+    data () {
+      return {
+        msg: 'Welcome to About Page'
+      }
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  h1{
+    margin-top: 2em;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  .img-about{
+  width: 500px;
+  margin-top: 4em;
+
+}
+  </style>
+  
