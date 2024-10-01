4f21f47d12add7dd973b1a04203d776f65350c43
Mayk29 <144456367+Mayk29@users.noreply.github.com>
2024-10-01 23:58:31 +0800

    Add files via upload

---
+++ view_router_demo/finalprj/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img src="./assets/naruto_logo.png" class="head-img">
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/about">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/gallery">Gallery</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/contact">Contact</router-link>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+    <router-view/>
+    
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
