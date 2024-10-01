a086ae843b59b74531ee2b665ddd40d9b3aea41b
Mayk29 <144456367+Mayk29@users.noreply.github.com>
2024-10-02 00:08:33 +0800

    Create aboutUs.vue

---
+++ view_cli_demo/vue-app-demo/src/components/aboutUs.vue
@@ -0,0 +1,34 @@
+<template id="about">
+    <div id="about-us">
+        <div id="todo-card">
+            <div class="container">
+                <div class="alert alert-primary" role="alert">
+                    <h1>{{ msg6 }}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"about-us",
+    data (){
+        return{
+            msg6:'Creating SPA'
+        }
+    }
+}
+</script>
+
+<style>
+#about{
+    background-color: #e6f7ff;
+}
+
+h1{
+    color: #fff;
+    text-align: center;
+}
+
+</style>
